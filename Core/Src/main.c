d953f265097fb6c589528fff15652b37255b3a1b
eduardo.schutz <eduardo.schutz@outlook.com>
2026-09-17 00:05:07 -0300

    feat: implement state machine structure

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,9 +21,17 @@
 #include "adc.h"
 #include "dma.h"
 #include "i2c.h"
+#include "stm32f3xx_hal.h"
+#include "stm32f3xx_hal_adc.h"
+#include "stm32f3xx_hal_adc_ex.h"
+#include "stm32f3xx_hal_def.h"
+#include "stm32f3xx_hal_gpio.h"
+#include "stm32f3xx_hal_tim.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
+#include <stdint.h>
+#include <stdio.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -33,10 +41,37 @@
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
 
+// Conxtexto que será compartilhado entre os estados
+typedef struct {
+} Context;
+
+// Definição do código de retorno dos estados
+typedef enum {
+  SETUP_STATE,               // Estado de incialização
+  READ_SENSOR_STATE,         // Leitura do sensor de luminosidade
+  READ_POT_STATE,            // Leitura do potenciômetro via ADC
+  CONTROL_STATE,             // Calcula o PWM
+  APPLY_PWM_STATE,           // Atualiza o PWM do LED
+  UPDATE_INDICATORS_STATE,   // Atializa os 4 LEDs indicadores
+  WAIT_STATE,                // Aguarda o tempo de resposta
+  ERROR_STATE,               // Trata o erro
+} StateId;
+
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+
+// Porcentagens do PWM
+#define PWM_0_PCT 0
+#define PWM_25_PCT 250
+#define PWM_50_PCT 500
+#define PWM_75_PCT 750
+#define PWM_90_PCT 900
+
+#define ARR 999
+
+#define ERROR_BLINK_DELAY_MS 250
 
 /* USER CODE END PD */
 
@@ -49,16 +84,229 @@
 
 /* USER CODE BEGIN PV */
 
+uint16_t sensor_value = 0;
+
+uint16_t adc_buffer[16];
+uint16_t pot_value = 0;
+
+uint16_t pwm_target = 0;
+uint16_t pwm_current = 0;
+
+uint16_t reponso_delay_ms = 100;
+uint16_t last_update_ms = 0;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
 
+// Protótipo da função estado
+typedef StateId StateFunction(Context *ctx) ;
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+// Função para obter a média dos valores do potenciometro
+uint16_t get_pot_average(uint16_t *vector)
+{
+  uint16_t sum = 0;
+  for (uint16_t i = 0; i < 16; i++)
+  {
+    sum += vector[i] >> 4; // divide por 16, equivalente a deslocar 4 bits para a direita
+  }
+
+  return sum;
+}
+
+// Função do estado de setup
+StateId state_setup(Context *ctx) 
+{
+  printf("[INFO] Starting system...\n");
+
+  // Incia o PWM do TIM3 CH1
+  if (HAL_TIM_PWM_Start(TIM_HandleTypeDef *htim3, uint32_t Channel) != HAL_OK) {
+    printf("[ERROR] Erro starting TIM3 PWM\n");
+    return ERROR_STATE;
+  }
+
+  // Inicia o PWM desligado
+  __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, PWM_0_PCT);
+
+  pwm_current = 0;
+  pwm_target = 0;
+
+  // Incia com os LEDs desligados
+  HAL_GPIO_WritePin(LED_D2_GPIO_Port, LED_D2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LED_D3_GPIO_Port, LED_D3_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LED_D4_GPIO_Port, LED_D4_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LED_D5_GPIO_Port, LED_D5_Pin, GPIO_PIN_RESET);
+
+  printf("[INFO] System OK\n");
+
+  return READ_SENSOR_STATE;
+}
+
+// Função do estado de leitura do sensor
+StateId state_read_sensor(Context *ctx) 
+{
+  printf("[INFO] Reading sensor...\n");
+
+  // Lê o sensor de luminosidade
+  // Implementar função para ler o sensor sensor_read(&sensor_value)
+  if (HAL_OK != HAL_OK) {
+    printf("[ERROR] Erro reading sensor\n");
+    return ERROR_STATE;
+  }
+
+  printf("[INFO] Brightness: %u\n", sensor_value);
+
+  return READ_POT_STATE;
+}
+
+// Função do estado de leitura do potenciometro
+StateId state_read_pot(Context *ctx) 
+{
+  printf("[INFO] Reading potentiometer...\n");
+
+  // Obtêm o valor
+  pot_value = get_pot_average(adc_buffer);
+
+  printf("[INFO] Potentiometer: %u\n", pot_value);
+
+  // Transforma o adc em milisegudos (20ms a 500ms)
+  reponso_delay_ms = 20 + ((uint32_t)pot_value * 480) / 4095;
+
+  printf("[INFO] Response time: %u\n", pot_value);
+
+  return CONTROL_STATE;
+}
+
+// Função do estado de controle e cálculo do PWM
+StateId state_control(Context *ctx)
+{
+  printf("[INFO] Controlling PWM...\n");
+
+  // Converte o nível do sensor em em CCR (duty cycle)
+  pwm_target = ARR - ((uint32_t)sensor_value * ARR) / 4095;
+
+  // limita o valor
+  if (pwm_target > 999)
+    pwm_target = 999;
+
+  printf("[INFO] Current: %u | Target: %u\n", pwm_current, pwm_target);
+
+  return APPLY_PWM_STATE;
+}
+
+// Função do estado de aplicação do PWM
+StateId state_apply_pwm(Context *ctx)
+{
+  printf("[INFO] Applying PWM...\n");
+
+  // Reduz o PWM aos poucos
+  if (pwm_current < pwm_target)
+  {
+    pwm_current++;
+  }
+  else if (pwm_current > pwm_target)
+  {
+    pwm_current--;
+  }
+
+  // Atualiza o valor do CCR
+  __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, pwm_current);
+
+  printf("[INFO] TIM channel 1 CRR: %u\n", pwm_current);
+
+  return UPDATE_INDICATORS_STATE;
+}
+
+//Função do estado de atualização dos indicadores
+StateId state_update_indicators(Context *ctx)
+{
+  printf("[INFO] Updating indicators...\n");
+
+  // Verifica qual o LED que deve ser ligado e desliga os demais
+  
+  // 25% do PWM
+  if (pwm_current >= PWM_25_PCT)
+  {
+    HAL_GPIO_WritePin(LED_D2_GPIO_Port, LED_D2_Pin, GPIO_PIN_SET);
+  }
+  else 
+  {
+    HAL_GPIO_WritePin(LED_D2_GPIO_Port, LED_D2_Pin, GPIO_PIN_RESET);
+  }
+  
+  // 50% do PWM
+  if (pwm_current >= PWM_50_PCT)
+  {
+    HAL_GPIO_WritePin(LED_D3_GPIO_Port, LED_D3_Pin, GPIO_PIN_SET);
+  }
+  else 
+  {  
+    HAL_GPIO_WritePin(LED_D3_GPIO_Port, LED_D3_Pin, GPIO_PIN_RESET);
+  }
+  
+  // 75% do PWM
+  if (pwm_current >= PWM_75_PCT)
+  {
+    HAL_GPIO_WritePin(LED_D4_GPIO_Port, LED_D4_Pin, GPIO_PIN_SET);
+  }
+  else 
+  {
+    HAL_GPIO_WritePin(LED_D4_GPIO_Port, LED_D4_Pin, GPIO_PIN_RESET);
+  }
+  
+  // 90% do PWM
+  if (pwm_current >= PWM_90_PCT)
+  {
+    HAL_GPIO_WritePin(LED_D5_GPIO_Port, LED_D5_Pin, GPIO_PIN_SET);
+  }
+  else 
+  {
+    HAL_GPIO_WritePin(LED_D5_GPIO_Port, LED_D5_Pin, GPIO_PIN_RESET);
+  }
+
+  return WAIT_STATE;
+}
+
+// Função do estado de aguardo do tempo de leitura do sensor
+StateId state_wait(Context *ctx)
+{
+  // Verifica se o tempo atual é maior que o tempo de resposta
+  if((HAL_GetTick() - last_update_ms) >= reponso_delay_ms)
+  {
+    // Atualiza o ultima atualização
+    last_update_ms = HAL_GetTick();
+
+    return READ_SENSOR_STATE;
+  }
+
+  return APPLY_PWM_STATE;
+}
+
+// Função do estado de erro
+StateId state_error(Context *ctx)
+{
+  printf("[ERROR] Error\n");
+
+  // Deliga o PWM 
+  __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, PWM_0_PCT);
+
+  // Começa a piscar os LEDs indicadores
+  while (1) {
+    HAL_GPIO_TogglePin(LED_D2_GPIO_Port, LED_D2_Pin);
+    HAL_GPIO_TogglePin(LED_D3_GPIO_Port, LED_D3_Pin);
+    HAL_GPIO_TogglePin(LED_D4_GPIO_Port, LED_D4_Pin);
+    HAL_GPIO_TogglePin(LED_D5_GPIO_Port, LED_D5_Pin);
+
+    HAL_Delay(ERROR_BLINK_DELAY_MS);
+  }
+}
 
 /* USER CODE END 0 */
 
@@ -70,6 +318,23 @@ int main(void)
 {
 
   /* USER CODE BEGIN 1 */
+
+  // Inicia tipos
+  Context ctx;
+  StateId currentState = SETUP_STATE;
+
+  // Define tabela de estados
+  StateFunction *state_table[] = {
+    [SETUP_STATE] = state_setup,
+    [READ_SENSOR_STATE] = state_read_sensor,
+    [READ_POT_STATE] = state_read_pot,
+    [CONTROL_STATE] = state_control,
+    [APPLY_PWM_STATE] = state_apply_pwm,
+    [UPDATE_INDICATORS_STATE] = state_update_indicators,
+    [WAIT_STATE] = state_wait,
+    [ERROR_STATE] = state_error
+  }
+
 
   /* USER CODE END 1 */
 
@@ -99,6 +364,10 @@ int main(void)
   MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
 
+  // Inicializa DMA para a leitura do potenciometro
+  HAL_ADCEx_Calibration_Start(&hadc1, ADC_SINGLE_ENDED);
+  HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_buffer, 16);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -108,7 +377,12 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+
+    // Inicia máquina de estados
+    currentState = state_table[currentState](&ctx);
   }
+
+  return 0;
   /* USER CODE END 3 */
 }
 
